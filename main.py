846c4730a6ccc900362210f55457f133a0301660
calebkim107 <calebkim1117@gmail.com>
2020-08-06 19:38:16 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,58 @@
+# VARIABLE - stores a piece of data  
+
+# a = 2 #integer 
+# b = "2" #string
+
+# print(a+a)
+# print(b+b)
+
+# What are the two characterstics of data? VALUE and DATA Type 
+
+# LOOP - peats code for a given amount of times or until a condition is set.
+
+# For loop
+# for i in range(5):
+#   print("Its almost friday", i)
+
+# while True
+# smelly = True
+# while smelly == True:
+  # print("i smell")
+  # smelly = False
+
+#   counter = 0
+#   while counter < 5:
+#     print(counter)
+#     counter += 1
+#     # Conditionals - IF/ELSE     
+# if counter  == 0:
+#   print("equals 0")
+# elif counter > 5:
+#     print("greater than 5")
+# else:
+#   print("less than 5")
+
+
+
+e = 3 #integer
+b = "3" #string
+
+print(e+e) #this should equal 6
+print(b+b) #this should equal 33
+
+for i in range(7):
+  print("Its almost the end of summer")
+
+
+soccer = True
+while soccer == True:
+  print("soccer ball")
+  soccer = False
+
+  num = 0
+  name = "Caleb"
+
+  for i in range(3):
+    print("hello")
+
+    status = 
